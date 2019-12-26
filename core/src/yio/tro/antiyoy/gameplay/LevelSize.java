51981baaeb80bb18b57fa38510ad4d657a1007bb
Ruichka <42318266+Ruichka@users.noreply.github.com>
2019-12-27 01:51:53 +0300

    Update LevelSize.java

--- core/src/yio/tro/antiyoy/gameplay/LevelSize.java
+++ core/src/yio/tro/antiyoy/gameplay/LevelSize.java
@@ -8,7 +8,7 @@ public class LevelSize {
     public static final int MEDIUM = 2;
     public static final int BIG = 4;
     public static final int HUGE = 9;
-    public static final int GIANT = 16
+    public static final int GIANT = 16;
 
 
     public static String convertToString(int index) {
